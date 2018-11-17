261ebaa596cb970f365fe0fff58517d187a5a6fc
yijiushenjing <1146947499@qq.com>
2018-11-17 14:25:56 +0800

    2

--- bid-platform/src/main/java/com/bid/springcloud/Platform.java
+++ bid-platform/src/main/java/com/bid/springcloud/Platform.java
@@ -4,15 +4,19 @@ package com.bid.springcloud;/*
 
 */
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoDataAutoConfiguration.class})
 @EnableEurekaClient
 @EnableFeignClients
+@MapperScan(basePackages = "com.bid.springcloud.mapper")
 public class Platform {
     public static void main(String[] args) {
         SpringApplication.run(Platform.class,args);
--- bid-platform/src/main/java/com/bid/springcloud/controller/DeptController_Feign.java
+++ bid-platform/src/main/java/com/bid/springcloud/controller/DeptController_Feign.java
@@ -4,23 +4,29 @@ import java.util.List;
 
 import com.bid.springcloud.entities.CoUser;
 import com.bid.springcloud.entities.Dept;
-import com.bid.springcloud.service.DeptClientService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bid.springcloud.service.CoUserService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 
 @RestController
 public class DeptController_Feign
 {
-    @Autowired
-    private DeptClientService service = null;
 
+    @Resource
+    private CoUserService coUserService;
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return  "Hello";
+    }
     @RequestMapping(value = "/consumer/dept/get/{id}")
-    public CoUser get(@PathVariable("id") Integer id)
-    {
-        return this.service.get(id);
+    public CoUser get(@PathVariable Integer id){
+        System.out.println("get resolver");
+       return  coUserService.get(id);
     }
 
 }
